ff4df30cfea012c46a24bc3ef711ab6c6120798b
zetsuboii <hmzkrb@gmail.com>
2023-03-01 21:10:55 +0300

    implement eq and cmp for u256

--- src/finite_fields/u256.rs
+++ src/finite_fields/u256.rs
@@ -1,2 +1,42 @@
-pub struct U256;
+pub struct U256 {
+    inner: [u64; 4],
+}
 
+impl PartialEq for U256 {
+    fn eq(&self, other: &Self) -> bool {
+        self.inner
+            .iter()
+            .zip(other.inner.iter())
+            .all(|(self_part, other_part)| self_part == other_part)
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(other)
+    }
+}
+
+impl PartialOrd for U256 {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        for (self_part, other_part) in self.inner.iter().zip(other.inner.iter()) {
+            if self_part > other_part {
+                return Some(std::cmp::Ordering::Greater);
+            } else if self_part < other_part {
+                return Some(std::cmp::Ordering::Less);
+            }
+        }
+        return Some(std::cmp::Ordering::Equal);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_eq() {
+        todo!()
+    }
+
+    #[test]
+    fn test_cmp() {
+        todo!()
+    }
+}
